886979ab5caa775c9cade918e9e7871b21d91db9
Riveong <setsubi9@gmail.com>
2024-03-19 00:52:06 +0700

    navbar

--- suryamedia/src/routes/+page.svelte
+++ suryamedia/src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script>
     import Mekanisme from "./mekanisme.svelte";
+    import Nav from "./nav.svelte";
 
 </script>
+<Nav />
 <Mekanisme />
---
+++ suryamedia/src/routes/nav.svelte
@@ -0,0 +1,47 @@
+<ul>
+    <li style="float:left"><b class="item">Surya Media Advertising</b></li>
+    <li><a href="/produk"><b>About</b></a></li>
+    <li><a href="/"><b>Contact</b></a></li>
+    <li><a href="/produk"><b>Product</b></a></li>
+    <li><a href="/produk"><b>Home</b></a></li>
+    
+  </ul>
+<style>
+        ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 20px;
+        overflow: hidden;
+
+    }
+
+    li {
+        float: right;
+    }
+
+    li a {
+        display: block;
+        color: rgb(0, 0, 0);
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+    }
+
+    .item{
+        display: block;
+        color: rgb(0, 0, 0);
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+    }
+
+    li a:hover{
+        color: #FBA000;
+
+    }
+
+    li a {
+        color: #623F20;
+    }
+
+</style>
